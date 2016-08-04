30b4318b4a9e7cd2e45c67b9297fa0c0877fdb45
EvilCult <Ray@Rays-Macbook-Pro-Office.local>
2016-08-04 16:52:40 +0800

    ...

--- site/resources/views/Manage/editor_menu.php
+++
@@ -1,4 +0,0 @@
-					<ul class="nav nav-pills nav-stacked" id="leftMenu">
-						<li role="presentation"><a href="/editor/list" class="text-center bg-info" menu="editor/list">编辑列表</a></li>
-						<li role="presentation"><a href="/editor/add" class="text-center bg-info" menu="editor/add">添加编辑</a></li>
-					</ul>
